186b2911b158740fdcea754368ceedd63b937581
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-08-12 17:59:21 -0300

    applying consign to pass 'app' to each controllers module

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const consign = require('consign');
 
 const app = express();
+consign()
+  .include('controllers')
+  .into(app);
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000.');
 });
 
-app.get('/atendimentos', (req, res) => {
-  res.send('você está na rota de atendimentos!')
-});
