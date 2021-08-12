4dbf173d710ac2458e3a62d1e0258e08394c4ee0
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-08-12 17:55:02 -0300

    config nodemon to restart the server in each project change

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000.');
 });
 
-app.get('/', (req, res) => {
-  res.send('servidor rodando!')
-})
+app.get('/atendimentos', (req, res) => {
+  res.send('você está na rota de atendimentos!')
+});
