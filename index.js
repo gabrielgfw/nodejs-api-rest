d617d7ce30b404e3178bf70912806c877afdc2c6
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-08-13 15:08:14 -0300

    creating 'atendimentos' table

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const customExpress = require('./config/customExpress');
 const conexao = require('./infraestrutura/conexao');
+const Tabelas = require('./infraestrutura/Tabelas');
 
 // Contém as configurações do App.
 const app = customExpress();
@@ -10,14 +11,20 @@ conexao.connect((error) => {
     console.log('Erro ao conectar no banco de dados.');
     console.error(error);
 
-  // Caso a conexão com o banco de dados tenha sido bem sucedida
-  // será inicializado o App.
+  // Caso a conexão com o banco de dados tenha sido bem sucedida,
+  // será feita uma checagem e se necessário as tabelas serão criadas.
+  // Após a criação das tabelas o servidor node será inicializado.
   } else {
     console.log('Banco de dados conectado com sucesso.');
+    inicializarTabelas(conexao);
     executarApp();
   }
 });
 
+
+function inicializarTabelas(conexao) {
+  Tabelas.init(conexao);
+}
 
 function executarApp() {
   app.listen(3000, () => {
